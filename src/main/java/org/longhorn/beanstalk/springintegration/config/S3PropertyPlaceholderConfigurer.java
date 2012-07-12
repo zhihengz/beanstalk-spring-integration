bb6195f06a7a8c27074d83f3c35309f10de0bf17
Todd Nine <tnine@apigee.com>
2012-07-12 12:25:15 -0700

    Updated tests and documentation

--- src/main/java/org/longhorn/beanstalk/springintegration/config/S3PropertyPlaceholderConfigurer.java
+++ src/main/java/org/longhorn/beanstalk/springintegration/config/S3PropertyPlaceholderConfigurer.java
@@ -1,59 +1,60 @@
 package org.longhorn.beanstalk.springintegration.config;
 
-import java.util.List;
-import java.util.Properties;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Properties;
 
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
-import org.springframework.context.ResourceLoaderAware;
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
 
-public class S3PropertyPlaceholderConfigurer extends PropertyPlaceholderConfigurer {
+public class S3PropertyPlaceholderConfigurer extends
+        PropertyPlaceholderConfigurer {
 
     private S3ResourceLoader resourceLoader;
-    private String[] s3Locations = new String[ 0 ];
-    private Resource[] conventionalResources = new Resource[ 0 ];
+    private String[] s3Locations = new String[0];
+    private Resource[] conventionalResources = new Resource[0];
 
     public S3PropertyPlaceholderConfigurer() {
         resourceLoader = new S3ResourceLoader();
     }
 
-    public void setLocations( Resource[] locations ) {
+    public void setLocations(Resource[] locations) {
         this.conventionalResources = locations;
     }
-    public void setS3Locations( String[] s3Locations ) {
-        this.s3Locations = new String[ s3Locations.length ];
-        for ( int i = 0; i < s3Locations.length; i++ ) {
-            this.s3Locations[i] = parseStringValue( s3Locations[i], 
-                                                    new Properties(), 
-                                                    new HashSet() );
+
+    @SuppressWarnings("deprecation")
+    public void setS3Locations(String[] s3Locations) {
+        this.s3Locations = new String[s3Locations.length];
+        for (int i = 0; i < s3Locations.length; i++) {
+            this.s3Locations[i] = parseStringValue(s3Locations[i],
+                    new Properties(), new HashSet<String>());
         }
 
     }
 
-    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory ) throws BeansException {
+    public void postProcessBeanFactory(
+            ConfigurableListableBeanFactory beanFactory) throws BeansException {
         injectS3Resources();
-        super.postProcessBeanFactory( beanFactory );
+        super.postProcessBeanFactory(beanFactory);
     }
 
-    private void injectS3Resources( ) {
-        
+    private void injectS3Resources() {
+
         int total = conventionalResources.length + s3Locations.length;
-       
-        if ( total > 0 ) {
+
+        if (total > 0) {
             List<Resource> allResources = new ArrayList<Resource>();
-            for ( Resource conventionalResource : conventionalResources ) {
-                allResources.add( conventionalResource );
+            for (Resource conventionalResource : conventionalResources) {
+                allResources.add(conventionalResource);
             }
-            for ( String s3Location : s3Locations ) {
-                allResources.add( resourceLoader.getResource( s3Location ) );
+            for (String s3Location : s3Locations) {
+                allResources.add(resourceLoader.getResource(s3Location));
             }
-            super.setLocations( allResources.toArray( new Resource[0] ) );
+            super.setLocations(allResources.toArray(new Resource[0]));
         }
-        
+
     }
 }
---
+++ src/test/java/org/longhorn/beanstalk/springintegration/config/S3ResourceLoadTest.java
@@ -0,0 +1,80 @@
+package org.longhorn.beanstalk.springintegration.config;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assume.assumeTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class S3ResourceLoadTest {
+
+    private ApplicationContext context = null;
+
+    @Before
+    public void setUp() {
+
+        if (!areAWSCredentialsInSystemProperties()) {
+            printOutTestSkipMessage();
+            assumeTrue(false);
+        }
+        System.clearProperty("foo");
+    }
+
+    private boolean areAWSCredentialsInSystemProperties() {
+
+        return System.getProperty("AWS_ACCESS_KEY_ID") != null
+                && System.getProperty("AWS_SECRET_KEY") != null;
+    }
+
+    private void printOutTestSkipMessage() {
+
+        System.out.println("++++++++++++++++++++++++++++++++++++++++++++++");
+        System.out.println("+ No AWS credentails found, skip tests       +");
+        System.out.println("++++++++++++++++++++++++++++++++++++++++++++++");
+    }
+
+    @Test
+    public void testLoadPropertiesFromS3() {
+        context = new ClassPathXmlApplicationContext(
+                new String[] { "testContext.xml" });
+        TestBean testBean = (TestBean) context.getBean("testBean");
+        assertEquals("bar", testBean.getFoo());
+    }
+
+    @Test
+    public void testLoadPropertiesFromLocations() {
+        context = new ClassPathXmlApplicationContext(
+                new String[] { "load_from_locations_context.xml" });
+        TestBean testBean = (TestBean) context.getBean("testBean");
+        assertEquals("bar", testBean.getFoo());
+    }
+
+    @Test
+    public void testLoadPropertiesFromEmbeddedProperties() {
+        context = new ClassPathXmlApplicationContext(
+                new String[] { "load_from_embeded_properties_context.xml" });
+        TestBean testBean = (TestBean) context.getBean("testBean");
+        assertEquals("bar", testBean.getFoo());
+    }
+
+    @Test
+    public void testFetchS3LocationsFromSystemProperties() {
+        System.setProperty("s3locations", "s3://com-ea2d-tmp/s3.properties");
+        context = new ClassPathXmlApplicationContext(
+                new String[] { "s3location_from_system_properties_context.xml" });
+        TestBean testBean = (TestBean) context.getBean("testBean");
+        assertEquals("bar", testBean.getFoo());
+    }
+
+    @Test
+    public void testSystemOverride() {
+        System.setProperty("foo", "world");
+        context = new ClassPathXmlApplicationContext(
+                new String[] { "testContext.xml" });
+        TestBean testBean = (TestBean) context.getBean("testBean");
+        assertEquals("world", testBean.getFoo());
+    }
+
+}
